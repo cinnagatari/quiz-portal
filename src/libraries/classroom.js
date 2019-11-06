67a92bc0442771b17a9e00d9c689af71e87442a0
David Chang <cinnagatari@gmail.com>
2019-11-06 11:19:12 -0800

    testing moment

--- src/libraries/classroom.js
+++ src/libraries/classroom.js
@@ -3,73 +3,72 @@ import idb from "../utils/indexedDB";
 import { db, firebase } from "../utils/firebase";
 
 const classroom = {
-  add: async function addClass(newClass) {
-    let versionState = {};
-    await version.check(["u", "co"]).then(v => (versionState = v));
-  },
+    add: async function addClass(newClass) {
+        let versionState = {};
+        await version.check(["u", "co"]).then(v => (versionState = v));
+    },
 
-  edit: async function editClass(newClass, original) {},
+    edit: async function editClass(newClass, original) {},
 
-  delete: async function deleteClass(original) {},
+    delete: async function deleteClass(original) {},
 
-  addStudent: async function addStudent(newStudent, classroom) {
-    let versionState = {};
-    await version.check("u", "co").then(v => (versionState = v));
-    let users = [];
-    let idbUsers = await idb.users.get("users").then(u => u);
-    if (idbUsers === undefined || versionState.u === "load") {
-      await loadDB.users(u => {
-        users = u;
-      });
-    } else {
-      users = idbUsers.users;
-    }
-    let classes = [];
-    let idbClasses = idb.classes.get("classes").then(c => c);
-    if (idbClasses === undefined || versionState.co === "load") {
-      await loadDB.classes(c => (classes = c));
-    } else {
-      classes = idbClasses.classes;
-    }
-    let usernames = users.map(u => u.username);
-    let newCourse = {};
-    newStudent.courses.forEach(c => {
-      if (c.name === classroom.name) newCourse = c;
-    });
-
-    console.log(usernames);
-    console.log("" + usernames.includes(newStudent.username));
-    console.log("" + !classroom.students.includes(newStudent.username));
-    console.log(
-      "" + newCourse.classes !== undefined ||
-        !newCourse.classes.includes(classroom.time)
-    );
-    if (
-      usernames.includes(newStudent.username) &&
-      !classroom.students.includes(newStudent.username) &&
-      (newCourse.classes === undefined ||
-        !newCourse.classes.includes(classroom.time))
-    ) {
-      console.log(1);
-      classroom.students.push(newStudent.username);
-      if (newCourse.course === undefined) {
-        newCourse = { course: classroom.course, classes: [classroom.time] };
-        newStudent.courses = [...newStudent.courses, newCourse];
-      } else {
-        newStudent.courses = newStudent.courses.map(c => {
-          if (c.course === classroom.course)
-            return { ...c, classes: [...c.classes, classroom.time] };
-          else return c;
+    addStudent: async function addStudent(newStudent, classroom) {
+        let versionState = {};
+        await version.check("u", "co").then(v => (versionState = v));
+        let users = [];
+        let idbUsers = await idb.users.get("users").then(u => u);
+        if (idbUsers === undefined || versionState.u === "load") {
+            await loadDB.users(u => {
+                users = u;
+            });
+        } else {
+            users = idbUsers.users;
+        }
+        let classes = [];
+        let idbClasses = idb.classes.get("classes").then(c => c);
+        if (idbClasses === undefined || versionState.co === "load") {
+            await loadDB.classes(c => (classes = c));
+        } else {
+            classes = idbClasses.classes;
+        }
+        let usernames = users.map(u => u.username);
+        let newCourse = {};
+        newStudent.courses.forEach(c => {
+            if (c.name === classroom.name) newCourse = c;
         });
-      }
-      console.log(newStudent);
-      // await db
-      //   .collection("users")
-      //   .doc(newStudent.username)
-      //   .set(newStudent);
-    } else {
+        if (
+            usernames.includes(newStudent.username) &&
+            !classroom.students.includes(newStudent.username) &&
+            (newCourse.classes === undefined ||
+                !newCourse.classes.includes(classroom.time))
+        ) {
+            console.log(1);
+            classroom.students.push(newStudent.username);
+            if (newCourse.course === undefined) {
+                newCourse = {
+                    course: classroom.course,
+                    classes: [classroom.time]
+                };
+                newStudent.courses = [...newStudent.courses, newCourse];
+            } else {
+                newStudent.courses = newStudent.courses.map(c => {
+                    if (c.course === classroom.course)
+                        return {
+                            ...c,
+                            classes: [...c.classes, classroom.time]
+                        };
+                    else return c;
+                });
+            }
+            console.log(newStudent);
+            console.log(classroom);
+            // await db
+            //   .collection("users")
+            //   .doc(newStudent.username)
+            //   .set(newStudent);
+        } else {
+        }
     }
-  }
 };
 
 export { classroom };
